04958c89282244c4207f43443005b0085745a9cc
Jacob Ratima <jacobratima@gmail.com>
2022-05-27 17:20:04 +1200

    .

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -36,7 +36,7 @@ const Skills = () => {
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
-                  <p className='my-4'>React</p>
+                  <p className='my-4'>Angular</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Github} alt="HTML icon" />
