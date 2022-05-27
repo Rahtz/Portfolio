08ff8f7c29f009fff1750dc30b496d8233b3bf33
Jacob Ratima <jacobratima@gmail.com>
2022-05-27 16:57:29 +1200

    initial

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Navbar from "./components/NavBar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
