2ea8274a0a8f0b889514b256cc4479bc6da051be
Jacob Ratima <jacobratima@gmail.com>
2022-05-30 21:40:59 +1200

    .

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -54,7 +54,7 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/Rahtz/BlockCity-Frontend">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
